4dc20f2bf9be5d53e928b9b95cc9a5e1ef261b42
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-13 16:22:38 +0200

    add layout details

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,18 +5,18 @@
 
 @section('main-content')
 <section>
-    <div class="container mt-5 text-center">
+    <div class="container my-5 text-center">
         <h1 class="mb-5">Laravel Models Controller</h1>
        <div class="row g-4">
         @foreach($movies as $movie)
         <div class="col-md-4">
             <div class="card text-center">
                 <div class="card-body">
-                  <h3 class="card-title">{{$movie->title}}</h3>
-                  <h5 class="card-title">{{$movie->original_title}}</h5>
-                  <p class="card-text">{{$movie->nationality}}</p>
-                  <p class="card-text">{{$movie->date}}</p>
-                  <p class="card-text">{{$movie->vote}}</p>
+                  <h3 class="custom card-title border-bottom mb-3 pb-3">{{$movie->title}}</h3>
+                  <h5 class="card-title"><span class="fw-bold me-1 text-danger">Original title:</span> {{$movie->original_title}}</h5>
+                  <p class="card-text fw-bold">{{$movie->nationality}}</p>
+                  <p class="card-text"><span class="fw-bold me-1 text-danger"> Date:</span> {{$movie->date}}</p>
+                  <p class="card-text"><span class="fw-bold me-1 text-danger"> Vote:</span>{{$movie->vote}}</p>
                 </div>
             </div>
         </div>
