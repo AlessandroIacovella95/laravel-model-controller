a2e1fb22ed2da562c9201d353884b28e24484a2a
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-13 15:58:41 +0200

    add cards

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+// use Illuminate\Http\Request;
 use App\Models\Movie;
-use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-
+ public function index(){
+    $movies = Movie::all();
+    return view('homepage', compact('movies'));
+ }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,28 @@
 
+@extends('layouts.app')
+
+@section('page-name', 'Homepage')
+
+@section('main-content')
+<section>
+    <div class="container mt-5 text-center">
+        <h1 class="mb-5">Laravel Models Controller</h1>
+       <div class="row g-4">
+        @foreach($movies as $movie)
+        <div class="col-md-4">
+            <div class="card text-center">
+                <div class="card-body">
+                  <h3 class="card-title">{{$movie->title}}</h3>
+                  <h5 class="card-title">{{$movie->original_title}}</h5>
+                  <p class="card-text">{{$movie->nationality}}</p>
+                  <p class="card-text">{{$movie->date}}</p>
+                  <p class="card-text">{{$movie->vote}}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+       </div>
+    </div>
+</section>
+@endsection
+
